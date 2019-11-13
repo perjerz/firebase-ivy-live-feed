1731b23cb08b911c7767cdec70d7298ecd7e45c4
perjerz3434 <perjerz3434@gmail.com>
2019-11-13 11:43:27 +0700

    add like state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ interface Post {
 export class AppComponent implements OnInit {
   posts$: Observable<Post[]>;
   user$: Observable<firebase.User>;
+  myLike: boolean;
 
   constructor(
     private matDialog: MatDialog,
@@ -33,52 +34,15 @@ export class AppComponent implements OnInit {
     private afs: AngularFirestore,
     private auth: AngularFireAuth,
     private appService: AppService,
-  ) {}
+  ) { }
 
-  trackByKey(index: number) {
-    return index;
-  }
   ngOnInit() {
     this.user$ = this.auth.user;
     this.posts$ = this.afs.collection<Post>('posts').valueChanges();
   }
 
-  private pickImage() {
-    const input: HTMLInputElement = document.createElement('input');
-    input.type = 'file';
-    input.accept = 'image/jpeg, image/png';
-    // TODO: test input click because it doesn't work in some browsers
-    input.click();
-    return fromEvent(input, 'change').pipe(
-      switchMap(() => {
-        const file = input.files[0];
-        if (file.size > MAX_FILE_SIZE) {
-          return throwError('Too large file. File must be less than 4 MB.');
-        }
-        return of(file);
-      })
-    );
-  }
-
-  private readFileToBase64(file: File) {
-    const reader = new FileReader();
-    const readFile$ = (fileReader: FileReader) => {
-      return fromEvent(fileReader, 'load').pipe(take(1));
-    };
-    reader.readAsDataURL(file);
-    return readFile$(reader).pipe(map(() => reader.result as string));
-  }
-
-  private checkImageError(base64: string) {
-    const img: HTMLImageElement = document.createElement('img');
-    img.src = base64;
-    return race(
-      fromEvent(img, 'error').pipe(
-        take(1),
-        switchMap(() => throwError('You image is failed to load.'))
-      ),
-      timer(IMAGE_LOAD_TIMEOUT).pipe(map(() => base64))
-    );
+  trackByKey(index: number) {
+    return index;
   }
 
   openFeedDialog() {
@@ -131,5 +95,52 @@ export class AppComponent implements OnInit {
       .catch(err => {
         this.matSnackbar.open(err.toString());
       });
+  }
+
+  like() {
+    this.myLike = !this.myLike;
+    // TODO: Optimistic update
+  }
+
+  share() {
+    // TODO: Web Share API
+  }
+
+  private pickImage() {
+    const input: HTMLInputElement = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/jpeg, image/png';
+    // TODO: test input click because it doesn't work in some browsers
+    input.click();
+    return fromEvent(input, 'change').pipe(
+      switchMap(() => {
+        const file = input.files[0];
+        if (file.size > MAX_FILE_SIZE) {
+          return throwError('Too large file. File must be less than 4 MB.');
+        }
+        return of(file);
+      })
+    );
+  }
+
+  private readFileToBase64(file: File) {
+    const reader = new FileReader();
+    const readFile$ = (fileReader: FileReader) => {
+      return fromEvent(fileReader, 'load').pipe(take(1));
+    };
+    reader.readAsDataURL(file);
+    return readFile$(reader).pipe(map(() => reader.result as string));
+  }
+
+  private checkImageError(base64: string) {
+    const img: HTMLImageElement = document.createElement('img');
+    img.src = base64;
+    return race(
+      fromEvent(img, 'error').pipe(
+        take(1),
+        switchMap(() => throwError('You image is failed to load.'))
+      ),
+      timer(IMAGE_LOAD_TIMEOUT).pipe(map(() => base64))
+    );
   }
 }
