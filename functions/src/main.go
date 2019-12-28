9360a8c78e9e033c43d51c8ee9ae31e01ffd6ac0
perjerz3434 <perjerz3434@gmail.com>
2019-12-28 23:22:23 +0700

    fix main.go for test

--- functions/src/main.go
+++ functions/src/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"cloud.google.com/go/firestore"
 	"context"
+	"errors"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"firebase.google.com/go/storage"
+	"fmt"
+	"google.golang.org/api/option"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
-
-	firebase "firebase.google.com/go"
-	"google.golang.org/api/option"
 )
 
 // Post is the post document store in FireStore
@@ -19,87 +25,201 @@ type post struct {
 	LikeUserIDs []string `json:"likeUserIds" firestore:"likeUserIds"`
 }
 
+var (
+	firestoreClient *firestore.Client
+	storageClient   *storage.Client
+	authClient      *auth.Client
+	ctx             context.Context
+)
+
+func init() {
+	ctx = context.Background()
+	opt := option.WithCredentialsFile("./key.json")
+	config := &firebase.Config{
+		ProjectID:     "fir-ivy-live-feed",
+		StorageBucket: "fir-ivy-live-feed.appspot.com",
+		DatabaseURL:   "https://fir-ivy-live-feed.firebaseio.com",
+	}
+	app, err := firebase.NewApp(ctx, config, opt)
+	if err != nil {
+		log.Fatalf("firebase.NewApp: %v", err)
+		return
+	}
+
+	storageClient, err = app.Storage(ctx)
+	if err != nil {
+		log.Fatalf("app.Storage: %v", err)
+		return
+	}
+
+	firestoreClient, err = app.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("app.Firestore: %v", err)
+		return
+	}
+
+	authClient, err = app.Auth(ctx)
+	if err != nil {
+		firestoreClient.Close()
+		log.Fatalf("app.Auth: %v", err)
+		return
+	}
+}
+
+func parseToken(r *http.Request) (*auth.Token, error) {
+	auth := r.Header.Get("Authorization")
+	cookie, _ := r.Cookie("__session")
+	if !strings.HasPrefix(auth, "Bearer ") && cookie == nil {
+		return nil, errors.New("Unauthorized")
+	}
+
+	var idToken string
+	if strings.HasPrefix(auth, "Bearer ") {
+		idToken = strings.TrimPrefix(auth, "Bearer ")
+	} else if cookie != nil {
+		idToken = cookie.Value
+	} else {
+		return nil, errors.New("Unauthorized")
+	}
+
+	token, err := authClient.VerifyIDToken(ctx, idToken)
+	return token, err
+}
+
+// Post is triggered by HTTP Request
+func Post(w http.ResponseWriter, r *http.Request) {
+
+	defer firestoreClient.Close()
+
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://fir-ivy-live-feed.web.app")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Max-Age", "7200")
+		return
+	}
+	if r.Method != http.MethodPost {
+		log.Fatalf("Wrong Method: %v", r.Method)
+		http.Error(w, "Accept POST only", http.StatusMethodNotAllowed)
+		return
+	}
+
+	token, err := parseToken(r)
+	if err != nil {
+		log.Fatalf("parseToken: %v", err.Error())
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, 4<<20)
+	err = r.ParseMultipartForm(5 << 20)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	f, fh, err := r.FormFile("image")
+
+	defer f.Close()
+
+	if err != nil {
+		log.Fatalf("File error: %v", err.Error())
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	buffer, err := ioutil.ReadAll(f)
+
+	if err != nil {
+		log.Fatalf("File error: %v", err.Error())
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	fileType := http.DetectContentType(buffer)
+
+	if !strings.HasPrefix(fileType, "image/") {
+		log.Fatalf("UID posts wrong image")
+		http.Error(w, "Image is not valid.", http.StatusBadRequest)
+		return
+	}
+
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		log.Fatalf("f.Seek: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	bh, err := storageClient.DefaultBucket()
+	if err != nil {
+		log.Fatalf("storageClient.DefaultBucket: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	obj := bh.Object("posts/" + fh.Filename)
+
+	sw := obj.NewWriter(ctx)
+
+	if _, err = io.Copy(sw, f); err != nil {
+		log.Fatalf("Object(fh.Filename).NewWriter: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := sw.Close(); err != nil {
+		log.Fatalf("sw.Close: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	attrs, err := bh.Attrs(ctx)
+	if err != nil {
+		log.Fatalf("bh.Attrs: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Attrs: %v %v", sw.Attrs().ContentType, sw.Attrs().Size)
+
+	u, err := url.Parse("/" + attrs.Name + "/" + sw.Attrs().Name)
+
+	if err != nil {
+		log.Fatalf("url.Parse: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// TODO: sanitize message
+	message := r.FormValue("message")
+	post := post{
+		UserID:      token.UID,
+		Image:       sw.Attrs().Name,
+		Message:     message,
+		LikeUserIDs: []string{},
+	}
+
+	_, _, err = firestoreClient.Collection("posts").Add(ctx, post)
+	if err != nil {
+		log.Fatalf(`Collection("posts").Add: %v`, err)
+		err2 := bh.Object("posts/" + fh.Filename).Delete(ctx)
+		if err2 != nil {
+			log.Fatalf("bucketHandle.Object(fh.Filename).Delete: %v", err)
+			http.Error(w, err2.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "https://fir-ivy-live-feed.web.app")
+	fmt.Fprintf(w, "Post successfully! URL: %s", u.EscapedPath())
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodOptions {
-			w.Header().Set("Access-Control-Allow-Origin", "https://localhost:4200")
-			w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
-			w.Header().Set("Access-Control-Allow-Methods", "POST")
-			w.Header().Set("Access-Control-Max-Age", "7200")
-			return
-		}
-		if r.Method != http.MethodPost {
-			log.Fatalf("Wrong Method: %v", r.Method)
-			http.Error(w, "Accept POST only", http.StatusMethodNotAllowed)
-			return
-		}
-
-		r.Body = http.MaxBytesReader(w, r.Body, 4<<20)
-		err := r.ParseMultipartForm(5 << 20)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		f, _, err := r.FormFile("image")
-
-		if err != nil {
-			log.Fatalf("File error: %v", err.Error())
-			http.Error(w, "", http.StatusBadRequest)
-			return
-		}
-
-		buffer, err := ioutil.ReadAll(f)
-
-		if err != nil {
-			log.Fatalf("File error: %v", err.Error())
-			http.Error(w, "", http.StatusInternalServerError)
-			return
-		}
-
-		fileType := http.DetectContentType(buffer)
-
-		if !strings.HasPrefix(fileType, "image/") {
-			log.Fatalf("UID posts wrong image")
-			http.Error(w, "Image is not valid.", http.StatusBadRequest)
-			return
-		}
-
-		message := r.FormValue("message")
-
-		log.Printf("message %v", message)
-		w.Header().Set("Access-Control-Allow-Origin", "https://localhost:4200")
-
-		ctx := context.Background()
-		opt := option.WithCredentialsFile("./key.json")
-		app, err := firebase.NewApp(ctx, nil, opt)
-		if err != nil {
-			log.Fatalf("firebase.NewApp: %v", err)
-			return
-		}
-
-		firestoreClient, err := app.Firestore(ctx)
-		if err != nil {
-			log.Fatalf("app.Firestore: %v", err)
-			return
-		}
-		post := post{
-			UserID:      "userid",
-			Image:       "name.jpg",
-			Message:     "message",
-			LikeUserIDs: []string{},
-		}
-
-		_, _, err = firestoreClient.Collection("posts").Add(ctx, post)
-		if err != nil {
-			log.Print(err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	})
+	mux.HandleFunc("/post", Post)
 
 	http.ListenAndServe(":8080", mux)
 }
